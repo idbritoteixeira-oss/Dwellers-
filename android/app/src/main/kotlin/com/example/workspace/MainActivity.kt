15612cc0d60eddba94dd1228485a0f0c2a0cb309
Gael <52140346-idbritoteixeira@users.noreply.replit.com>
2026-01-31 19:16:24 +0000

    reset: reconstruindo estrutura android do zero

--- android/app/src/main/kotlin/com/example/workspace/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/workspace/MainActivity.kt
@@ -2,6 +2,4 @@ package com.example.workspace
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    // Deixe vazio, o FlutterActivity moderno faz tudo sozinho
-}
+class MainActivity: FlutterActivity()
